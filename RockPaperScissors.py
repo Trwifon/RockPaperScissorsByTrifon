5b5ab40753385b67b05fa6cabb539534d1516a8c
Trwifon <143388684+Trwifon@users.noreply.github.com>
2023-09-22 23:20:46 +0300

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,14 @@
+rock = 'Rock'
+paper = 'Paper'
+scissors = 'Scissors'
+
+player_move = input("Choose [r]ock, [p]aper or [s]cissors:")
+
+if player_move == 'r':
+    player_move = rock
+elif player_move == 'p':
+    player_move = paper
+elif player_move == 's':
+    player_move = scissors
+
+print(player_move)
